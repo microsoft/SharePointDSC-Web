d0884af3d4bac3f677c09a34a9efb254aa2bcd27
Nik Charlebois <nik_charlebois@hotmail.com>
2018-11-13 19:47:02 -0800

    changes

--- SharePointDSC-Web/scripts/spdsc.js
+++ SharePointDSC-Web/scripts/spdsc.js
@@ -12,8 +12,45 @@ function AddServerNode(currentNumberOfServers)
     content += "<option value='WebFrontEndWithDistributedCache'>Web Front-End with Distributed Cache</option>";
     content += "<option value='WebFrontEnd'>Web Front-End</option>";
     content += "</select></td>";
-    content += "<td><a href='#' class='spdsc-addnode' onclick='AddServerNode(" + nextNode + ")'>";
+    content += "<td><a href='#' class='spdsc-addnode' onclick='AddServerNode(" + nextNode + "); return false;' id='spdsc-addserver" + nextNode + "'>";
     content += "<img src='images/plusicon.png' alt='Add a Server to the Farm' /></a></td></tr>";
     table.append(content);
-    $("#spdsc-addserver" + currentNumberOfServers).hide();
+    $("#spdsc-addserver" + currentNumberOfServers).html("<img src='images/checkmark.png' />");
+}
+
+function AddFarmAdmin(currentNumberofAdmins)
+{
+    var nextItem = currentNumberofAdmins + 1;
+    var content = "<br />";
+    content += "<input type='text' id='txtFarmAdmin" + nextItem + "' class='spdsc-widetextbox' />";
+    content += "<a href='#' class='spdsc-addnode' onclick='AddFarmAdmin(" + nextItem + ");return false;'>";
+    content += "<img src='images/plusicon.png' alt='Add a Farm Admin' id='spdsc-img-addfarmadmin" + nextItem + "' /></a>"
+    $("#spdsc-img-addfarmadmin" + currentNumberofAdmins).hide();
+    $("#spdsc-section-farmadmins").append(content);
+}
+
+function AddCustomSolution(currentNumberOfSolutions)
+{
+    var nextItem = currentNumberOfSolutions + 1;
+    var content = "<tr>";
+    content += "<td><input type='text' id='txtCustomSolutionPath" + nextItem + "' /></td>";
+    content += "<td><input type='checkbox' id='chckCustomSolutionDeployed" + nextItem + "' /></td>";
+    content += "<td><input type='text' id='txtCustomSolutionWebApps" + nextItem + "' /></td>";
+    content += "<td><input type='text' id='txtCustomSolutionCompatibility" + nextItem + "' /></td>";
+    content += "<td><a href='#' class='spdsc-addnode' onclick='AddCustomSolution(" + nextItem + ");return false;' id='spdsc-img-addcustomsolution" + nextItem + "'>";
+    content += "<img src='images/plusicon.png' alt='Add a Custom Solution' /></a></td></tr>";
+    $("#spdsc-table-customsolution").append(content);
+    $("#spdsc-img-addcustomsolution" + currentNumberOfSolutions).html("<img src='images/checkmark.png' />");
+}
+
+function Toggle(sectionName, show)
+{
+    if(show)
+    {
+        $("#" + sectionName).show();
+    }
+    else
+    {
+        $("#" + sectionName).hide();
+    }
 }
